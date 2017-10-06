a28342519aa29565d5fa01177acc9879180f6bbd
Mike Tsou <mr.tsou@gmail.com>
2017-10-05 22:19:29 -0700

    Add background image and implement responsive design

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const welcomeListener = function() {
   $('#content').hide();
   $('.one, .two, .three, .four').each(function(divs) {
     $(this).delay(divs * 1500).fadeIn(2000, function() {
-      $('#welcome').delay(5000).fadeOut(2000, function() {
+      $('#welcome').delay(1000).fadeOut(2000, function() {
         $('#welcome').remove();
         $('#content').fadeIn(1000);
       });
